2617a1d85b6d348d46d3fcd41e212c0af692976b
BlueSky1234 <yahshuaharris@icloud.com>
2024-09-09 08:37:03 -0700

    Added try/except and simplified code

--- Python 2 Projects/Contacts.py
+++ Python 2 Projects/Contacts.py
@@ -1,17 +1,25 @@
 contacts = []
-answer = "y"
-while answer == "y":
-    name = input()
-    contacts.append(name)
-    answer = input("Would you like to add another?").lower().strip()
-    if answer == "y":
-        continue
-    elif answer == "n":
-        
+updating = True
+while updating:
+    try:
+        print("New contact's name")
+        name = input().strip()
+        if name and not any(x in name for x in "\n\t\r"):
+            contacts.append(name)
+            answer = input("Would you like to add another?").lower().strip()
+            if answer == "y":
+                continue
+            elif not answer or answer == "n":
+                updating = False
+                break
+            else:
+                print("Invalid Input")
+                continue
+        else:
+            if contacts:
+                print("Contacts saved")
+                print("\n".join(name for name in sorted(contacts)))
+                updating = False
+                break
+    except KeyboardInterrupt: 
         break
-    else:
-        print("Invalid Input")
-        continue
-
-
-print(contacts.sort())
