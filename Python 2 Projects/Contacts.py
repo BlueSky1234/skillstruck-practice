64a7e378def50fc073dcd10a76bdaf180399474c
BlueSky1234 <yahshuaharris@icloud.com>
2024-11-08 10:29:48 -0700

    exported from Skill Struck

--- Python 2 Projects/Contacts.py
+++ Python 2 Projects/Contacts.py
@@ -1,25 +1,17 @@
 contacts = []
-updating = True
-while updating:
-    try:
-        print("New contact's name")
-        name = input().strip()
-        if name and not any(x in name for x in "\n\t\r"):
-            contacts.append(name)
-            answer = input("Would you like to add another?").lower().strip()
-            if answer == "y":
-                continue
-            elif not answer or answer == "n":
-                updating = False
-                break
-            else:
-                print("Invalid Input")
-                continue
-        else:
-            if contacts:
-                print("Contacts saved")
-                print("\n".join(name for name in sorted(contacts)))
-                updating = False
-                break
-    except KeyboardInterrupt: 
+answer = "y"
+while answer == "y":
+    name = input()
+    contacts.append(name)
+    answer = input("Would you like to add another?").lower().strip()
+    if answer == "y":
+        continue
+    elif answer == "n":
+        
         break
+    else:
+        print("Invalid Input")
+        continue
+
+
+print(contacts.sort())
