591d69061bfc1294f086db9728d029ac0a1e4f9e
BlueSky1234 <yahshuaharris@icloud.com>
2024-11-08 10:29:54 -0700

    exported from Skill Struck

---
+++ Python3/classes.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age 
+        self.gender = gender
+
+#def __init__( ) is "initialize"
+#__init__( ) needs the self parameter
